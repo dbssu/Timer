d1d1c55a73c73993122d25fb1cfdd94a56a62281
Yoonsu Kang <kangys0725@naver.com>
2019-05-21 15:34:33 +0900

    #2 타이머 컨트롤과 텍스트박스 컨트롤을 사용하여 카운트하는 카운트 기능 구현

--- Timer/Form1.Designer.cs
+++ Timer/Form1.Designer.cs
@@ -66,6 +66,7 @@
             // Timer
             // 
             this.Timer.Interval = 1000;
+            this.Timer.Tick += new System.EventHandler(this.Timer_Tick);
             // 
             // Form1
             // 
@@ -79,7 +80,6 @@
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "타이머";
-            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
